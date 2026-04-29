a7f5399cfb3eec291b60a1ec3bfe8e55b5842662
Parasu-21 <parasuraman2116@gmail.com>
2026-04-29 11:16:55 +0530

    Third-commit

--- day-3/ifelse.java
+++ day-3/ifelse.java
@@ -1,10 +1,25 @@
 public class ifelse {
     public static void main(String args[]) {
+        // Simple if-else
         int a = 10;
         if (a > 5) {
             System.out.println("a is greater than 5");
         } else {
             System.out.println("a is less than or equal to 5");
+        }
+
+        // Multiple if-else (if-else if-else)
+        int score = 85;
+        if (score >= 90) {
+            System.out.println("Grade: A");
+        } else if (score >= 80) {
+            System.out.println("Grade: B");
+        } else if (score >= 70) {
+            System.out.println("Grade: C");
+        } else if (score >= 60) {
+            System.out.println("Grade: D");
+        } else {
+            System.out.println("Grade: F");
         }
     }
 }
