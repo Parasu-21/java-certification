30284f1ff59b656394af76301b1385c9277745bd
Parasu-21 <parasuraman2116@gmail.com>
2026-04-29 14:43:41 +0530

    Sixth-commit

--- day-3/Scanuse.java
+++ day-3/Scanuse.java
@@ -2,10 +2,15 @@ import java.util.Scanner;
 public class Scanuse {
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
-        int n;
-        do{
+        int n = 1;
+        // do{
+        //     n = sc.nextInt();
+        //     System.out.println("You entered: " + n);
+        // }while(n!=0);
+
+        while(n!=0) {
             n = sc.nextInt();
             System.out.println("You entered: " + n);
-        }while(n!=0);
+        }
     }
 }
